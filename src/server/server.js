de863550eae74682a367552a7a25b39f8146aab3
Anjasfedo <fedoaf98@gmail.com>
2024-05-15 12:37:00 +0700

    Do// Load Model

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,7 @@
 const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+require("dotenv").config();
+const loadModel = require("../services/loadModel");
 
 const initServer = async () => {
   const server = Hapi.server({
@@ -10,8 +13,49 @@ const initServer = async () => {
       },
     },
   });
+  
+  server.route(routes);
+
+  const model = await loadModel();
+  server.app.model = model;
+
+
+  server.ext("onPreResponse", function (request, h) {
+    const response = request.response;
+
+    if (response instanceof InputError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: `${response.message} Silakan gunakan foto lain.`,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    if (response.isBoom) {
+      if (response.output.statusCode === 413) {
+        const newResponse = h.response({
+          status: "fail",
+          message:
+            "Payload content length greater than maximum allowed: 1000000",
+        });
+        newResponse.code(413);
+        return newResponse;
+      } else {
+        const newResponse = h.response({
+          status: "fail",
+          message: response.message,
+        });
+        newResponse.code(response.output.statusCode);
+        return newResponse;
+      }
+    }
+
+    return h.continue;
+  });
+
   await server.start();
   console.log(`Server start at: ${server.info.uri}`);
 };
 
-initServer()
+initServer();
